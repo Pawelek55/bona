53accf068dda2732e84f82dc169af80788017f74
Pawelek55 <pawelz.zawislak@gmail.com>
2018-08-18 18:47:23 +0200

    Welcome on the site!

--- app/js/opinion-slider.js
+++ app/js/opinion-slider.js
@@ -21,61 +21,20 @@ for (let i = 0; i < opinionSlideCount; i++) {
     opinionSlideIndex = opinionSlideIndex + 1;
 }
 
-/*slideIndex = 0;
-
-nextSlide.addEventListener('click', function () {
-    
-// clearInterval(this.time);
-//    this.time = setInterval(function() {
-//        this.slideNext();
-//    }.bind(this), 6000); //co 6 sekund automatycznie się przełączy
-
-    slideIndex = slideIndex + 1;
-    slider.style.marginLeft = slideIndex * -100 + '%';
-
-    if (slideIndex > 3) {
-        slideIndex = 0;
-        slider.style.marginLeft = slideIndex * -100 + '%';
-    }
-});
-
-previousSlide.addEventListener('click', function () {
-    slideIndex = slideIndex - 1;
-    slider.style.marginLeft = slideIndex * -100 + '%';
-    if (slideIndex == -1) {
-        slideIndex = slideCount - 1;
-        slider.style.marginLeft = slideIndex * -100 + '%';
-    }
-});*/
-
-/*let timeLoop = setInterval(nextTime, 3000);
-
-function nextTime() {
-    console.log('domek')
-    opinionSlideIndex = opinionSlideIndex + 2;
-    opinionSlider.style.marginLeft = opinionSlideIndex * -50 + '%';
-
-    if (opinionSlideIndex > 4) {
-        opinionSlideIndex = 0;
-        opinionSlider.style.marginLeft = opinionSlideIndex * -50 + '%';
-    }   
-}*/
-
 
 let marginLeft;
 let opinionNewSlideIndex
 let dotsContainer = document.querySelector('.slider-dots');
 let dots = document.querySelectorAll('.slider-dots-element');
 
-setInterval(slide, 5000);
+let bigInterval = setInterval(slide, 4000);
 
 function slide(e) {
     if (opinionSlideIndex > 5) {
         opinionSlideIndex = 0;
     }
 
-
-//    if (e.target == dotsContainer) return;
+    //    if (e.target == dotsContainer) return;
 
     //    let marginLeft = opinionSlideIndex * opinionSlideWidth + '%';
     marginLeft = opinionSlideIndex * -50 + '%';
@@ -90,22 +49,54 @@ function slide(e) {
         opinionSlideIndex = opinionNewSlideIndex;
     })
 
-    console.log(opinionSlideIndex)
+    //    console.log(opinionSlideIndex)
     let dotsArr = Array.from(dots)
 
-    if (opinionSlideIndex < 2) {
-        dotsArr[2].classList.remove('slider-dots-element-active');
-        dotsArr[0].classList.add('slider-dots-element-active');
-    } else if (opinionSlideIndex < 4) {
-        dotsArr[0].classList.remove('slider-dots-element-active');
-        dotsArr[1].classList.add('slider-dots-element-active');
-    } else {
-        dotsArr[1].classList.remove('slider-dots-element-active');
-        dotsArr[2].classList.add('slider-dots-element-active');
+}
+
+if (window.innerWidth < 768) {
+    opinionSlideIndex = 0;
+    opinionSlider.style.width = opinionSlideCount * 100 + '%';
+
+    for (let i = 0; i < opinionSlideCount; i++) {
+        opinionArr[i].style.marginLeft = opinionSlideIndex * opinionSlideWidth / 2 + '%';
+        if (i % 2 == 0) {
+            console.log(opinionArr[i])
+        } else {
+            opinionArr[i].style.marginLeft = opinionArr[i - 1].style.marginLeft;
+            console.log(opinionArr[i])
+        }
+        opinionSlideIndex = opinionSlideIndex + 1;
     }
 
-
 }
+
+function below(){
+if (window.innerWidth < 768) {
+    opinionSlideIndex = 0;
+    opinionSlider.style.width = opinionSlideCount * 100 + '%';
+
+    for (let i = 0; i < opinionSlideCount; i++) {
+        opinionArr[i].style.marginLeft = opinionSlideIndex * opinionSlideWidth / 2 + '%';
+        if (i % 2 == 0) {
+            console.log(opinionArr[i])
+        } else {
+            opinionArr[i].style.marginLeft = opinionArr[i - 1].style.marginLeft;
+            console.log(opinionArr[i])
+        }
+        opinionSlideIndex = opinionSlideIndex + 1;
+    }
+
+} else{
+        opinionSlider.style.width = opinionSlideCount * 50 + '%';
+    }
+}
+
+window.addEventListener('resize', below)
+
+
+
+
 
 // Buttons
 
@@ -157,3 +148,56 @@ dots.forEach(dot => dot.addEventListener('click', (e) => {
 
 }));
 */
+
+
+
+/*slideIndex = 0;
+
+nextSlide.addEventListener('click', function () {
+    
+// clearInterval(this.time);
+//    this.time = setInterval(function() {
+//        this.slideNext();
+//    }.bind(this), 6000); //co 6 sekund automatycznie się przełączy
+
+    slideIndex = slideIndex + 1;
+    slider.style.marginLeft = slideIndex * -100 + '%';
+
+    if (slideIndex > 3) {
+        slideIndex = 0;
+        slider.style.marginLeft = slideIndex * -100 + '%';
+    }
+});
+
+previousSlide.addEventListener('click', function () {
+    slideIndex = slideIndex - 1;
+    slider.style.marginLeft = slideIndex * -100 + '%';
+    if (slideIndex == -1) {
+        slideIndex = slideCount - 1;
+        slider.style.marginLeft = slideIndex * -100 + '%';
+    }
+});*/
+
+/*let timeLoop = setInterval(nextTime, 3000);
+
+function nextTime() {
+    console.log('domek')
+    opinionSlideIndex = opinionSlideIndex + 2;
+    opinionSlider.style.marginLeft = opinionSlideIndex * -50 + '%';
+
+    if (opinionSlideIndex > 4) {
+        opinionSlideIndex = 0;
+        opinionSlider.style.marginLeft = opinionSlideIndex * -50 + '%';
+    }   
+}*/
+
+/*    if (opinionSlideIndex < 2) {
+        dotsArr[2].classList.remove('slider-dots-element-active');
+        dotsArr[0].classList.add('slider-dots-element-active');
+    } else if (opinionSlideIndex < 4) {
+        dotsArr[0].classList.remove('slider-dots-element-active');
+        dotsArr[1].classList.add('slider-dots-element-active');
+    } else {
+        dotsArr[1].classList.remove('slider-dots-element-active');
+        dotsArr[2].classList.add('slider-dots-element-active');
+    }*/
